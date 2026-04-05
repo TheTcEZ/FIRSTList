df18b71d9208ac082cb43929c32c14f2b7da564b
TheTcEZ <84231363+TheTcEZ@users.noreply.github.com>
2026-04-05 17:44:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'firstlist-v1';
+const ASSETS = [
+  './firstlist.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
